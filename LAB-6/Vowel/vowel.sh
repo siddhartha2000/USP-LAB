2e1b55d4d44744996cab8805eeb2cf322f0fc08e
siddhartha2000 <53754692+siddhartha2000@users.noreply.github.com>
2020-11-03 15:33:20 +0530

    Add files via upload

---
+++ LAB-6/Vowel/vowel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p " Enter the character: " ch
+case $ch in 
+	"a") echo "It is a vowel" ;; 
+	"A") echo "It is a vowel" ;;	
+	"e") echo "It is a vowel" ;;
+	"E") echo "It is a vowel" ;;
+	"i") echo "It is a vowel" ;;
+	"I") echo "It is a vowel" ;;
+	"o") echo "It is a vowel" ;;
+	"O") echo "It is a vowel" ;;
+	"u") echo "It is a vowel" ;;
+	"U") echo "It is a vowel" ;;
+	 *) echo "It is a Consonant" 
+esac
