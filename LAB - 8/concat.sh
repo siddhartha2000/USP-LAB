ece9f6ed082d96b748cf5282a3ac20d3e552e954
siddhartha2000 <53754692+siddhartha2000@users.noreply.github.com>
2020-12-08 14:43:22 +0530

    Add files via upload

---
+++ LAB - 8/concat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Enter File1 and file2: " f1 f2
+
+rm file3.txt
+touch file3.txt 
+head -n 5 $f1 > file3.txt 
+head -n 15 $f2  | tail -n 8 >> file3.txt
+echo "the new file is:"
+cat file3.txt
